5090c6766ef0c5ba4fee74402949a89210d971ee
laurent <laurentmartins@outlook.com>
2019-02-12 11:44:14 +0100

    first commit

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,16 @@
+package dev;
+import com.github.lalyos.jfiglet.FigletFont;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String texteKFC = FigletFont.convertOneLine("KFC");
+
+        System.out.println(texteKFC);
+    }
+}
