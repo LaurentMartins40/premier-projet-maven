3428432aab55a5305388f5c1a3496a7b731e0630
laurent <laurentmartins@outlook.com>
2019-02-12 15:50:26 +0100

    héritage de maven

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,4 +1,6 @@
 package dev;
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -7,10 +9,15 @@ import com.github.lalyos.jfiglet.FigletFont;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        String texteKFC = FigletFont.convertOneLine("KFC");
-
-        System.out.println(texteKFC);
-    }
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre"); 
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+		
+		// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString(
+		"environnement");
+		System.out.println("Environnement : " + environnement);
+		
+		}
 }
